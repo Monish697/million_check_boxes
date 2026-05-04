97f890d6f4f1cb0a7c70676e82426824fda8fd83
monish punjwani <monishpunjwani55@gmail.com>
2026-05-04 16:47:11 +0530

    bug solved

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ async function main() {
     const app = express();
     const server = http.createServer(app);
     const PORT = process.env.PORT || 5000;
+    const noOfCheckBoxes = 1000;
+    let state = { checkBoxes: [] };
+    for (let i = 0; i <= noOfCheckBoxes; i++) {
+        //@ts-ignore
+        state.checkBoxes[i] = false;
+    }
 
     //Io.configuration --
     const io = new Server();
@@ -21,11 +27,19 @@ async function main() {
 
         socket.on(`client:checkbox:changed`, (data) => {
             console.log(data);
+            let indexToChangeState = data.message.split(" ")[0].split("-")[1];
+            let stateToUpdate = data.message.split(": ")[1];
+            //@ts-ignore
+            state.checkBoxes[Number(indexToChangeState)] = stateToUpdate;
             io.emit(`server:emited:data`, data);
         });
     });
     //Express --
     app.use(express.static(path.resolve("./public")));
+
+    app.get("/checkBoxes", (req, res) => {
+        return res.status(200).json(state);
+    });
 
     app.get("/health", (req, res) => {
         return res.status(200).json({ message: "Healthy" });
